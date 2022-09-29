53a2092a6cced8975588d78fc32ddfa8dc2b397d
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-09-29 17:11:31 +0500

    create react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="navbar">
+          <h4>Ahmed raza</h4>
+          <p> Ahmed Raza </p>
+        </div>
+      </div> 
+
+    </div>
+  );
+}
+
+export default App;
